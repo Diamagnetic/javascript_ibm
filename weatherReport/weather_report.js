00775829248bd93904173884be70e443ea2b086f
Chirag <c.d.marshall1869@gmail.com>
2024-06-15 18:32:38 -0400

    new fetch api added

--- weatherReport/weather_report.js
+++ weatherReport/weather_report.js
@@ -16,4 +16,24 @@ function showweatherDetails(event) {
     });
 }
 
+function showweatherDetails(event) {
+    event.preventDefault();
+
+    const latitude = document.getElementById('latitude').value;
+    const longitude = document.getElementById('longitude').value;
+
+    const apiKey = 'fad24c72e9d0b5b2eb95ad29d3a207da';
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
+
+    fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => {
+        const weatherInfo = document.getElementById('weatherInfo');
+        weatherInfo.innerHTML = `<h2>Weather in ${data.name}</h2>
+        <p>Temperature: ${data.main.temp - 273} &#8451;</p>
+        <p>Weather: ${data.weather[0].description}</p>`;
+    });
+}
+
 document.getElementById('weatherForm').addEventListener('submit',showweatherDetails );
+document.getElementById('weatherForm').addEventListener('weatherCoord', showweatherDetailsCoord);
